36496dde420f130426f19e05b4b9bdacea5ea00e
Sarah Pai <pai.sarah89@gmail.com>
2018-08-25 21:48:11 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -37,27 +37,6 @@ do {
 
 
 
-
-
-
-//  function forLoop(array){
-//  for (let i = 0; i < 25; i++){
-//    if (i === 1){
-//      array.push (`I am ` + i + ` strange loop.`)}
-//  else {
-//      array.push (`I am ${i} strange loops.`)}
-//  }
-//  return array
-//  }
-
-
-//  function whileLoop(n){
-//  while(n > 0){
-//  console.log (--n)
-//  }
-//   return 'done';
-//  }
-
 //  function doWhileLoop(array) {
   
 //    do {
