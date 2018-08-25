53cd477c2c462aab0d2972bc2f0d7a0a348b73ff
Sarah Pai <pai.sarah89@gmail.com>
2018-08-25 21:44:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,7 +17,15 @@ return 'done';
 }
 
 function doWhileLoop(array){
-  
+  var i = 0;
+function incrementVariable() {
+  i = i + 1;
+}
+ 
+do {
+  console.log("doo-bee-doo-bee-doo");
+  incrementVariable();
+} while (i < 5);
 }
 
 
